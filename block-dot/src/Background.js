28d0ac943c483df5437b4124cdd885c43b1468f4
anton-voron <anton.voron.gt@gmail.com>
2019-05-12 17:19:23 +0300

    position

--- block-dot/src/Background.js
+++ block-dot/src/Background.js
@@ -3,10 +3,20 @@ import background from './background.png';
 import './App.css';
 
 class Background extends Component {
+	constructor (props) {
+		super(props)
+		this.state = {
+			styles: {
+				position: "relative",
+				width: "500px",
+  				height: "500px",
+			}
+		}
+	}
 	render () {
 		return (
 			<div>
-				<img src={background} className="background" alt="background"/>
+				<img src={background} className="background" alt="background" style={this.state.styles}/>
 			</div>
 		);
 	}
--- block-dot/src/Form.js
+++ block-dot/src/Form.js
@@ -12,10 +12,16 @@ class Form extends Component {
 			    border: "2px solid black",
 			    fontWeight: "normal",
 
-			    width: "500px",
-			    minHeight: "100px",
+			    width: "200px",
+			    height: "100px",
 				},
 			isToggleOn: true,
+			wrapper: {
+				position: "absolute",
+				top: "35px",
+				left: "80px",
+			}
+
 		};
 		this.handleClick = this.handleClick.bind(this);
 	}
@@ -41,7 +47,9 @@ class Form extends Component {
 	}
 	render () {
 		return (
-			<section calssName="form-wrapper">
+			<section 
+			calssName="form-wrapper"
+			style={this.state.wrapper}>
 				 <h2>Text field</h2>
 				 <form calssName="form-wrapper">
 					<textarea 
